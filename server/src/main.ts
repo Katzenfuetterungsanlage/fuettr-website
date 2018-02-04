d5d262696b3cc7824622b475a937633bfe44ab2a
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-02-04 20:53:01 +0100

    test

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,16 +4,20 @@ import * as child from 'child_process';
 import * as fs from 'fs';
 import * as express from 'express';
 import * as bodyparser from 'body-parser';
-import * as debugsx from 'debug-sx';
+import * as morgan from 'morgan';
 
 const app = express();
+app.use(bodyparser.json());
+app.use(morgan('tiny'));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../updates.json'));
 });
-app.post('/', (req, res, next) => {
+app.post('/', (req, res) => {
   fs.appendFileSync(path.join(__dirname, '../updates.json'), req.body);
 });
 
 const port = 2526;
-http.createServer().listen(port);
+const server = http.createServer(app).listen(port, () => {
+  console.log('Server running on port ' + port);
+});
