e8eb881b34e1f4e26482fdbc20f35260e7554c3c
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-02-06 14:11:48 +0100

    auto-commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -24,8 +24,8 @@ app.post('/', (req: express.Request, res: express.Response, next: express.NextFu
   let newv: itf.Update = JSON.parse(JSON.stringify(req.body));
   newv.time = date.toLocaleTimeString();
   newv.date = date.toDateString();
-  let result: itf.Update[];
-  result.push(newv);
+  let result: itf.Root;
+  result.updates[0] = newv;
   for (let i = 0; i < file.updates.length; i++) {
     result.push(file.updates[i]);
   }
