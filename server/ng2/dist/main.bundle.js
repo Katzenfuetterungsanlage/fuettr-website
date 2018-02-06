a6679ed5c27ae3037e1461d83da9849392d4440a
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-02-06 08:39:20 +0100

    auto-commit

--- server/ng2/dist/main.bundle.js
+++ server/ng2/dist/main.bundle.js
@@ -38,7 +38,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div *ngIf=\"done\">\r\n  <div *ngFor=\"let update of Webhooks.updates\">\r\n    <div class=\"commit\">\r\n      <span style=\"font-size: 14pt\">\r\n        <pre>    </pre>{{update.head_commit.timestamp}}</span>\r\n      <br>\r\n      <b style=\"font-size: 16pt\">\r\n        <img [src]=\"update.sender.avatar_url\" width=\"20px\" class=\"avatar\"> {{update.pusher.name}}</b>\r\n      <br>\r\n      <a [href]=\"update.head_commit.url\" target=\"_blank\">{{update.head_commit.id}}</a>\r\n      <br>\r\n      <code>{{update.head_commit.message}}</code>\r\n    </div>\r\n  </div>\r\n</div>"
+module.exports = "<div *ngIf=\"done\">\r\n  <div *ngFor=\"let update of Webhooks.updates\">\r\n    <div class=\"commit\">\r\n      <span class=\"timestamp\">{{update.head_commit.timestamp}}</span>\r\n      <br>\r\n      <b class=\"name\">\r\n        <img [src]=\"update.sender.avatar_url\" width=\"20px\" class=\"avatar\"> {{update.pusher.name}}</b>\r\n      <br>\r\n      <a [href]=\"update.head_commit.url\" target=\"_blank\" class=\"url\">{{update.head_commit.id}}</a>\r\n      <br>\r\n      <code class=\"message\">{{update.head_commit.message}}</code>\r\n    </div>\r\n  </div>\r\n</div>"
 
 /***/ }),
 
