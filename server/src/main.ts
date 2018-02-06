7dda07a2979dfc1c474ee33f0fc481367f7db9ca
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-02-06 13:22:37 +0100

    auto-commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,8 @@ import * as express from 'express';
 import * as bodyparser from 'body-parser';
 import * as morgan from 'morgan';
 
+import * as itf from './interfaces';
+
 const privKey = fs.readFileSync(path.join(__dirname, '../../x509/server.pem'));
 const cert = fs.readFileSync(path.join(__dirname, '../../x509/server.crt'));
 const app = express();
@@ -18,7 +20,8 @@ app.get('/data', (req, res) => {
 });
 app.post('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
   let file = JSON.parse(fs.readFileSync(path.join(__dirname, '../updates.json')).toString());
-  file.updates.push(JSON.parse(JSON.stringify(req.body)));
+  let new = JSON.parse(JSON.stringify(req.body));
+  for (i=0; i<file.updates.le)
   fs.writeFileSync(path.join(__dirname, '../updates.json'), JSON.stringify(file));
   res.sendStatus(200);
 });
