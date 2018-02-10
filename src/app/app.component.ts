5e70f4deb55b564de96d937f3d6eca25a4d64aed
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-02-10 15:55:13 +0100

    whoop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, HostListener, ViewChild, Inject, ElementRef } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { PageScrollConfig, PageScrollService, PageScrollInstance, PageScrollOptions } from 'ng2-page-scroll';
 import { DOCUMENT } from '@angular/common';
@@ -10,17 +9,20 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   public navShow = false;
-  public active1 = false;
+  public active1 = true;
   public active2 = false;
   public active3 = false;
   public active4 = false;
   public active5 = false;
+  public tophidden = true;
+  public bottomhidden = false;
   private int = 1;
   @ViewChild('container') private container: ElementRef;
 
   constructor(private pageScrollService: PageScrollService, @Inject(DOCUMENT) private document: any, private titleService: Title) {}
 
   public ngOnInit() {
+    this.goTo('#home');
     this.titleService.setTitle('Füttr - Homepage');
     PageScrollConfig.defaultScrollOffset = -10;
     PageScrollConfig.defaultDuration = 200;
@@ -57,15 +59,28 @@ export class AppComponent implements OnInit {
     this.switcher();
   }
 
+  public up() {
+    this.int--;
+    this.switcher();
+  }
+
+  public down() {
+    this.int++;
+    this.switcher();
+  }
+
   private switcher() {
     this.active1 = false;
     this.active2 = false;
     this.active3 = false;
     this.active4 = false;
     this.active5 = false;
+    this.tophidden = false;
+    this.bottomhidden = false;
     switch (this.int) {
       case 1:
         this.active1 = true;
+        this.tophidden = true;
         this.goTo('#home');
         this.titleService.setTitle('Füttr - Homepage');
         break;
@@ -86,6 +101,7 @@ export class AppComponent implements OnInit {
         break;
       case 5:
         this.active5 = true;
+        this.bottomhidden = true;
         this.goTo('#contact');
         this.titleService.setTitle('Füttr - Kontakt');
         break;
